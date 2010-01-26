a486383e01dd6a3d92ce1cdec321973cc9ac09d8
Josh Dockery <jndockery@gmail.com>
2010-01-26 15:35:00 +0600

    Separation of some logic into classes, implementation of a robot list

--- zeemus.java
+++ zeemus.java
@@ -9,59 +9,132 @@ import java.util.*;
 
 public class Zeemus extends AdvancedRobot
 {
-
-
+	zRobot target;
+	zRobotList bots = new zRobotList();
+		
 	public void run() {
 		
-		
-	
+//		trackName = null;
 		
 		while(true) {
 			
 			setAdjustRadarForGunTurn(true);
 			setAdjustGunForRobotTurn(true);
-			turnRadarRight(360);
-			for(int i=0; i<12; i++){
-				back(10);
-				turnLeft(15);
-			}
-				
 			
+			//spin in a circle and scan
+		//	setBack(40000);
+		//	setTurnRight(90);
+			refresh();
+			
+			target = bots.getTarget();
+			aimFire(target);
 			
 		/*	getBattleFieldWidth();
 			getBattleFieldHeight();
 			getX();
 			getY();
-			
-			
-			
-			
 			*/
-		
-		
 		}
-	}
-
-
-	public void onScannedRobot(ScannedRobotEvent e) {
-		fire(1);
-	}
-
-
-
-	public void onHitByBullet(HitByBulletEvent e) {
-		turnLeft(90 - e.getBearing());
+		
 	}
 	
-	public void onDeath(DeathEvent e){
-		
+	public void aimFire(zRobot target)
+	{
+		double deg = target.zbearing;
+		turnGunRight(deg);
+		fire(1);
 	}
+	
+	
+	//When scanning a robot, just tell the zRobotList to deal with it
+	public void onScannedRobot(ScannedRobotEvent e) {
+		bots.zOnScan(e);
+	}
+	
+	public void onHitByBullet(HitByBulletEvent e) {
+	}
+	
+	public void refresh(){
+		turnRadarRight(360);
+	}
+	
+	public void reAcquire(){
+		turnRadarRight(360);
+	}
+	
 	
 }
 
 
 
+//Manages a list of robots
+class zRobotList{
+	public static ArrayList<zRobot> list;
+	
+	public zRobotList(){
+		list = new ArrayList<zRobot>();
+	}
+	
+	
+	//update positions of Robots based on scan event
+	public static void zOnScan(ScannedRobotEvent e)
+	{
+		zRobot a = new zRobot(e);
+		zRobot.zheading = 0;
+	}
+	
+	
+	//return the closest target
+	public static zRobot getTarget()
+	{
+		zRobot min = list.get(0);
+		for(zRobot r : list)
+		{
+			if(r.zdistance < min.zdistance)
+				min = r;
+		}
+		return min;
+	}
+}
 
+
+
+//contains information on each robot
+class zRobot{
+	public static double zheading;
+	public static double zbearing;
+	public static double zenergy;
+	public static double zdistance;
+	public static double zvelocity;
+	public static String zname;
+		
+	public zRobot(ScannedRobotEvent e){
+		zheading = e.getHeading();
+		zbearing = e.getBearing();
+		zenergy = e.getEnergy();
+		zdistance = e.getDistance();
+		zvelocity = e.getVelocity();
+		zname = e.getName();
+	}
+}
+
+
+
+
+/*	public static void directBullet(Bullet b){
+		double heading = 90;
+		double x = 200;
+		double y = 200;
+		double power = 3;
+		String ownerName = b.getName();
+		String victimName = b.getVictim();
+		boolean isActive = true;
+        int bulletId = b.bulletId;
+		
+		
+		
+		return new Bullet(heading, x, y, power, ownerName, victimName, isActive, bulletId);
+	}*/
 
 
 /*				//Reading files		
