48a74ad3698eab8548bb09c5ae1e56dcc6d166ad
Josh Dockery <jndockery@gmail.com>
2010-02-10 11:21:00 +0600

    Utilizing a map of hotspots (walls, enemies, etc.)

--- zeemus.java
+++ zeemus.java
@@ -2,7 +2,6 @@
 
 package JD;
 import robocode.*;
-import robocode.RobocodeFileOutputStream;
 import java.awt.*;
 import java.io.*;
 import java.util.*;
@@ -11,72 +10,98 @@ import java.util.*;
 public class Zeemus extends AdvancedRobot
 {
 	
-	zRobotList robotList = new zRobotList();
+	TargetList targets = new TargetList();
+	
+	//for setting up a map of dangerous locations
+	boolean initialized = false;
+	int[][] hotspots;
+	//the amount of pixels per index in the hotspot map
+	int GRANULARITY = 10;
 	
 	//define the radius for wall avoidance
-	public double radius = 250;
-	//define the max random wait between shot time
-	public int waitVariable = 30;
-	public int waitLeft;
-	public boolean waiting;
-	public boolean tookShot;
+	public double radius = 20;
+	
 		
 	public void run() 
 	{
-		
+		if(!initialized)
+			initialize();
+		System.out.println(hotspots.length +":"+hotspots[0].length);
 		while(true)
 		{
-			
 			setAdjustRadarForGunTurn(true);
 			setAdjustGunForRobotTurn(true);
-			Random rand = new Random();
 			
-			
-			//move foreward, turn in random directions, and spin the radar constantly
-			setTurnRadarRight(3600);
-			setAhead(40000);
-			if(rand.nextBoolean())
-			   setTurnRight(rand.nextInt(90));
-			else setTurnLeft(rand.nextInt(90));
-			
-			
+			setMovement();
+			setRadar();		
 			wallAvoidance();
-			
-			//shoot every once in a while
-			if(tookShot)
-				waitLeft = rand.nextInt(waitVariable);
-			if(waitLeft <=0)
-				waiting = false;
-			waitLeft--;		
+	
+		}
+	}
+	
+	public void initialize()
+	{
+		//define each location in the hotspot map to contain multiple pixels
+		hotspots = new int[(int)getBattleFieldHeight()/GRANULARITY][(int)getBattleFieldWidth()/GRANULARITY];
+		
+		//set up so that the edges of the map are 'dangerous'
+		for(int r=0; r< hotspots.length; r++){
+			for(int c=0; c< hotspots[r].length; c++){
+				hotspots[0][c] = 100;
+				hotspots[r][0] = 100;
+				hotspots[hotspots.length-1][c] = 100;
+				hotspots[r][hotspots[r].length-1] = 100;
+			}
 		}
 		
+		initialized = true;
 	}
-	//get the bearing of the robot, turn, and fire if you plan on shooting
-	public void aimFire(zRobot r)
+	
+	public void setMovement()
 	{
-		if(!waiting)
-		{
-			tookShot = true;
-			waiting = true;
-			stop();
-			double neededDir = (getHeading() + r.zbearing);
-			turnGunCorrectly(neededDir); 
-			fire(2);
-			resume();	
-		}			                                        
-	} 
+		ahead(100);
+	//	moveRandomly();	
+	}
+	
+	public void setRadar()
+	{
+		setTurnRadarRight(360);
+	}
 	
 	
-	//take robot's heading, velocity, and distace to robot
-	//estimate where they will be when the bullet arrives
-	public void estimateFuturePosition(zRobot r)
+	//BROKEN - causes 10000 calls to setXX method???
+	//move foreward, turn in random directions, and spin the radar constantly
+	public void moveRandomly()
 	{
-		
-		
-		
-		
-		
+		Random rand = new Random();
+		setAhead(40000);
+		if(rand.nextBoolean())
+		   setTurnRight(rand.nextInt(90));
+		else setTurnLeft(rand.nextInt(90));
 	}
+	
+	public void wallAvoidance()
+	{
+		//if within one radii of the wall, turn around
+		if(getX() < radius || getY() < radius || getX() > getBattleFieldWidth()-radius || getY() > getBattleFieldHeight()-radius)
+   		{
+    		stop();
+    		turnRight(180);
+    		ahead(100);
+    		resume();
+    	}
+	}
+	
+	public void aimFire(Location loc)
+	{
+		stop();
+		Location here = new Location(getX(), getY());
+		turnGunCorrectly(here.degreeTo(loc));
+		fire(1);
+		resume();
+	}
+	
+
 	
 	//optimize turning for right and left, but don't turn if < n degrees difference
 	public void turnGunCorrectly(double dir)
@@ -93,7 +118,7 @@ public class Zeemus extends AdvancedRobot
 	    boolean right = true;
 	    if(dir < 0)
 	    {
-	    	dir *=-1;
+	    	dir *= (-1);
 	    	right = !right;
 	    }
 	    else if(dir > 180){
@@ -107,128 +132,97 @@ public class Zeemus extends AdvancedRobot
 	    return;
 	}
   
-  //does not work for backwards motion
-     public void wallAvoidance()
-  {
-  	//store variables temporarily to decrease method calls
-  	double tempHeading = getHeading();
-  	double tempX = getX();
-  	double tempY = getY();
-  	
-    //if approaching a corner, turn around
-    
-    //top left
-    if(tempX < 2*radius && tempY < 2*radius)
-    	turnGunCorrectly(45);
-    //bottom right
-    if(tempX < 2*radius && tempY > getBattleFieldWidth() - radius)
-    	turnGunCorrectly(135);
-    //top left
-    if(tempX > getBattleFieldHeight() - radius && tempY < 2*radius)
-    	turnGunCorrectly(225);
-    //top right
-    if(tempX > getBattleFieldHeight() - radius && tempY > getBattleFieldWidth() - radius)
-    	turnGunCorrectly(315);
-
-    //if within one radii of the wall, begin turning around
-    //turn around at least 90 degrees, and a full 180 if you are 
-    //approaching straight on
-    
-    //else is present not because the criteria are exclusive, but only because
-    //it would not be good to enact both turning behaviors   	
-    else if(tempX < radius)
-    {
-      if(tempHeading >= 270)
-        turnRight(Math.abs(Math.cos(tempHeading))*90+90);
-      else 
-      	turnLeft(Math.abs(Math.cos(tempHeading))*90+90);
-    }           
-    else if(tempY < radius)
-    {
-      if(tempHeading >= 180)
-        turnRight(Math.abs(Math.sin(tempHeading))*90+90);
-      else 
-      	turnLeft(Math.abs(Math.sin(tempHeading))*90+90);
-    }
-    else if(tempX > getBattleFieldWidth()-radius)
-    {
-      if(tempHeading >= 90)
-        turnRight(Math.abs(Math.cos(tempHeading))*90+90);
-      else 
-      	turnLeft(Math.abs(Math.cos(tempHeading))*90+90);
-    }	
-    else if(tempY > getBattleFieldHeight()-radius)
-    {
-      if(tempHeading > 0 && tempHeading < 180)
-        turnRight(Math.abs(Math.sin(tempHeading))*90+90);
-      else 
-      	turnLeft(Math.abs(Math.sin(tempHeading))*90+90);
-    }
-
-    ahead(100);
-  }	
-	//When scanning a robot, just tell the zRobotList to deal with it
+	//When scanning a robot, just tell the BotLog to deal with it
 	public void onScannedRobot(ScannedRobotEvent e) 
 	{
-		robotList.logRobot(e, getX(), getY(), getHeading());
-		aimFire(robotList.getTarget());
+		targets.logRobot(e, getX(), getY(), getHeading());
+		aimFire(targets.getTarget().getLocation());
 	}
      
 	
 }
 
-
-
-//Manages a list of robots
-
-class zRobotList
+class TargetList
 {
-	public static ArrayList<Stack<zRobot>> list;
+	public static ArrayList<BotLog> log;
+		
+	public TargetList(){
+		log = new ArrayList<BotLog>();
+	}	
 	
-	public zRobotList()
+	public static boolean isEmpty()
 	{
-		list = new ArrayList<Stack<zRobot>>();
+		return log.size()<1;
 	}
 	
-	
-	//update positions of Robots based on scan event
 	public static void logRobot(ScannedRobotEvent e, double x, double y, double h)
 	{
 		boolean newbot = true;
-		for(int i=0; i<list.size(); i++)
+		for(int i=0; i<log.size(); i++)
 		{
 			//if this robot is already in the list then update him
-			if(list.get(i).peek().zname.equals(e.getName()))
+			if(log.get(i).getName().equals(e.getName()))
 			{
 				newbot = false;
-				list.get(i).push(new zRobot(e,x,y,h));
-				
+				log.get(i).logRobot(e,x,y,h);
 			}
 		}
 		
 		if(newbot){
-			Stack<zRobot> temp = new Stack<zRobot>();
-			temp.push(new zRobot(e,x,y,h));
-			list.add(temp);
+			log.add(new BotLog(e,x,y,h));
 		}
 	}
-
 	
-	//return the closest target
-	public static zRobot getTarget()
+	
+	//take robot's heading, velocity, and distace to robot
+	//estimate where they will be when the bullet arrives
+	public static Location getFiringPosition()
+	{
+		return new Location(0,0);
+	}
+		
+		
+			//return the closest target
+	public static Bot getTarget()
 	{
 		if(isEmpty())
 			return null;
-		
-		//check for the smallest distance
-		zRobot min = list.get(0).peek();
-		for(Stack<zRobot> s : list)
-		{
-			if(s.peek().zdistance < min.zdistance)
-				min = s.peek();
-		}
-		return min;
-		
+		else return log.get(0).getLast();
+	}
+}
+
+
+
+//Manages a list of events for each robot
+class BotLog
+{
+	public static ArrayList<Bot> list;
+	private static String name;
+	
+	public BotLog()
+	{
+		list = new ArrayList<Bot>();
+		name = null;
+	}
+	
+	public BotLog(ScannedRobotEvent e, double x, double y, double h)
+	{
+		list = new ArrayList<Bot>();
+		name = e.getName();
+		logRobot(e,x,y,h);
+	}
+	
+	public static void logRobot(ScannedRobotEvent e, double x, double y, double h)
+	{
+		list.add(new Bot(e,x,y,h));		
+	}
+	
+	public static Bot getLast(){
+		return list.get(0);
+	}
+	
+	public static String getName(){
+		return name;
 	}
 	
 	public static boolean isEmpty()
@@ -238,114 +232,100 @@ class zRobotList
 }
 
 
-
-//contains information on each robot, z will denote logged robot variables
-//variables are public static so they can be accessed without a 'get()'
-class zRobot
+//container for storing information on scanned robots
+class Bot
 {
-	public static double zheading;
-	public static double zbearing;
-	public static double zenergy;
-	public static double zdistance;
-	public static double zvelocity;
-	public static location zloc;
-	public static String zname;
+	private static double heading;
+	private static double bearing;
+	private static double energy;
+	private static double distance;
+	private static double velocity;
+	private static long time;
+	private static Location loc;
 		
-	public zRobot(ScannedRobotEvent e , double x, double y, double heading)
+	public Bot(ScannedRobotEvent e , double x, double y, double heading)
 	{
-		zheading = e.getHeading();
+		heading = e.getHeading();
 		//don't store bearing once fire on location is fixed
-		zbearing = e.getBearing();
-		zenergy = e.getEnergy();
-		zdistance = e.getDistance();
-		zvelocity = e.getVelocity();
-		zname = e.getName();
-		zloc = new location(zbearing, zdistance, x, y, heading);
+		bearing = e.getBearing();
+		energy = e.getEnergy();
+		distance = e.getDistance();
+		velocity = e.getVelocity();
+		loc = new Location(bearing, distance, x, y, heading);
 				
+	}
+	
+	public static double getHeading(){
+		return heading;
+	}
+	public static double getBearing(){
+		return bearing;
+	}
+	public static double getEnergy(){
+		return energy;
+	}
+	public static double getDistance(){
+		return distance;
+	}
+	public static double getVelocity(){
+		return velocity;
+	}
+	public static Location getLocation(){
+		return loc;
 	}
 	
 }
 
 //has a constructor which determines position from an onScannedEvent
-class location
+class Location
 {
 	public static double x;
 	public static double y;
 	
-	location(double newX, double newY)
+	Location(double newX, double newY)
 	{
 		x = newX;
 		y = newY;
 	}
 	
 	//calculate from heading, bearing, distance, and current location
-	location(double zbearing, double zdistance, double newX, double newY, double heading)
+	Location(double bearing, double distance, double newX, double newY, double heading)
 	{
 		
 		//calculate their heading
-		double zdirection = heading+zbearing;
+		double direction = heading+bearing;
 		
 		//add the x and y components of their distance to your x and y
-		x = newX + zdistance * Math.sin(Math.toRadians(zdirection));
-		y = newY + zdistance * Math.cos(Math.toRadians(zdirection));
+		x = newX + distance * Math.sin(Math.toRadians(direction));
+		y = newY + distance * Math.cos(Math.toRadians(direction));
 	}
 	
-	public static double getX()
+	//returns what degree points to the location specified from this location
+	public static double degreeTo(Location loc)
 	{
+		double angle = 180 + Math.toDegrees(Math.atan((x - loc.getX()) / (y - loc.getY())));
+		if(y < loc.getY())
+			angle = (angle + 180)%360;
+		return angle;
+	}
+	
+	public static double getX(){
 		return x;
 	}
-	public static double getY()
-	{
+	public static double getY(){
 		return y;
 	}
 	
 }
 
-
-class debug
-{
-	public static PrintStream ps;
-	
-	debug()
-	{
-		ps = null;
-	}
-	
-	public static void print(String input)
-	{
-		try {
-			new PrintStream(new RobocodeFileOutputStream(getDataFile("debug.dat")));
-			
-			ps.println("");
-			
-			if (ps.checkError()) {
-				System.out.println("I could not write the count!");
-			}
-		} catch (IOException e) {
-			System.out.println("IOException trying to write: ");
-			e.printStackTrace(System.out);
-		}
-		finally {
-			if (ps != null) {
-				ps.close();
-			}
-		}
-	}
-}
-
-
-
-
-
-
 	
                      /********* Code snippets
 
     	              //aim fire for locations, broken, always fires at 0
-	public void aimFire(zRobot r)
+	public void aimFire()
 	{
 		stop();
-		location loc = r.zloc;
+		location loc = new Location(0,0);
 		double neededDir = Math.tan((loc.getY() - getY()) / (loc.getX() - getX()));
 		turnGunCorrectly(neededDir);
 		fire(1);
@@ -395,29 +375,82 @@ class debug
 	
 	
 	
-	
+	//////////////////////////////
 	
 			
-  public void wallAvoidance()
+
+  
+     public void wallAvoidance()
   {
-    //if within one radii of the wall, begin turning around
-    if(getX() < radius || getY() < radius || getX() > getBattleFieldWidth()-radius || getY() > getBattleFieldHeight()-radius)
-    {
-    stop();
-      if(getHeading() <= 270)
-      {
-        turnRight(180);
-      }
-      else 
-      {
-      turnRight(180);
-      }
-      ahead(100);
-      resume();
-    }               
+  	//store variables temporarily to decrease method calls
+  	double tempHeading = getHeading();
+  	double tempX = getX();
+  	double tempY = getY();
+  	
+    //if approaching a corner, turn around
     
-  }
-			
+    //top left
+    if(tempX < 2*radius && tempY < 2*radius)
+    	turnLeft(180);
+    //bottom right
+    if(tempX < 2*radius && tempY > getBattleFieldWidth() - radius)
+    	turnLeft(180);;
+    //top left
+    if(tempX > getBattleFieldHeight() - radius && tempY < 2*radius)
+    	turnLeft(180);
+    //top right
+    if(tempX > getBattleFieldHeight() - radius && tempY > getBattleFieldWidth() - radius)
+    	turnLeft(180);
+
+    //if within one radii of the wall, begin turning around
+    //turn around at least 90 degrees, and a full 180 if you are 
+    //approaching straight on
+    
+    //else is present not because the criteria are exclusive, but only because
+    //it would not be good to enact both turning behaviors   	
+    else if(tempX < radius)
+    {
+      if(tempHeading >= 270)
+        turnRight(Math.abs(Math.cos(tempHeading))*90+90);
+      else 
+      	turnLeft(Math.abs(Math.cos(tempHeading))*90+90);
+    }           
+    else if(tempY < radius)
+    {
+      if(tempHeading >= 180)
+        turnRight(Math.abs(Math.sin(tempHeading))*90+90);
+      else 
+      	turnLeft(Math.abs(Math.sin(tempHeading))*90+90);
+    }
+    else if(tempX > getBattleFieldWidth()-radius)
+    {
+      if(tempHeading >= 90)
+        turnRight(Math.abs(Math.cos(tempHeading))*90+90);
+      else 
+      	turnLeft(Math.abs(Math.cos(tempHeading))*90+90);
+    }	
+    else if(tempY > getBattleFieldHeight()-radius)
+    {
+      if(tempHeading > 0 && tempHeading < 180)
+        turnRight(Math.abs(Math.sin(tempHeading))*90+90);
+      else 
+      	turnLeft(Math.abs(Math.sin(tempHeading))*90+90);
+    }
+
+    ahead(100);
+  }	
+  	
+			///////////////////////////////////////
+				//get the bearing of the robot, turn, and fire
+	public void aimFire(Bot r)
+	{
+		stop();
+		double neededDir = (getHeading() + r.getBearing());
+		turnGunCorrectly(neededDir); 
+		fire(2);
+		resume();	                                
+	} 
+	////////////////////////////////////////
 			
 			
 			******/
